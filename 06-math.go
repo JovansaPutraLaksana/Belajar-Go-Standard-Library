ccddda045528b13d94ddd1baa8c16a1a5e29ba19
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-02-08 21:47:56 +0700

    math

---
+++ 06-math.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// package math provides basic constants and mathematical functions.
+func main() {
+	// Demonstrate some math functions and constants
+	fmt.Println("Pi:", math.Pi)
+	fmt.Println("Square root of 16:", math.Sqrt(16))
+	fmt.Println("Power of 2^3:", math.Pow(2, 3))
+	fmt.Println("Absolute value of -42:", math.Abs(-42))
+	fmt.Println("Sine of Pi/2:", math.Sin(math.Pi/2))
+	fmt.Println("Ceiling of 3.14:", math.Ceil(3.14))
+	fmt.Println("Floor of 3.14:", math.Floor(3.14))
+}
